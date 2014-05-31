c960c68421fd95665ec53ad8915200d84255a313
wandeg <jmsmaringa@gmail.com>
2014-05-31 21:30:09 +0300

    Cron for stats

--- cronActions.py
+++ cronActions.py
@@ -1,4 +1,12 @@
 import webapp2
-import pydelicious 
+import logging
+from Workers import *
+from pydelicious import get_popular,get_userposts,get_urlposts
 
-class DeliciousStats(webapp2.RequestHandler):
+class FetchStats(webapp2.RequestHandler):
+	def get(self,tag):
+		# stats=FetchDeliciousStats(tag)
+		# logging.info(stats)
+		pass
+
+
