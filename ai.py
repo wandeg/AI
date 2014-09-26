21b81416d5c363754db5cd79e96d97881a13bbde
wandeg <jmsmaringa@gmail.com>
2014-09-26 21:47:43 +0300

    Uncommits
    (may cause bugs)

--- ai.py
+++ ai.py
@@ -1,8 +1,12 @@
 import webapp2
 import views
+import cronActions
 
 
 application = webapp2.WSGIApplication(
 	[
     webapp2.Route('/', handler=views.MainPage),
+
+    #cron
+    webapp2.Route('/cron/fetch_stats/<tag>', handler=cronActions.FetchStats)
 ], debug=True)
