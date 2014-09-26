2f7ec26f69de6e802da507fdf17bdabe05d72cee
wandeg <jmsmaringa@gmail.com>
2014-09-26 13:35:37 +0300

    A few changes

--- ai.py
+++ ai.py
@@ -1,6 +1,18 @@
 import webapp2
 import views
 
+from google.appengine.api import users
+from google.appengine.ext import ndb
+
+import jinja2
+
+
+
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
 
 application = webapp2.WSGIApplication(
 	[
--- views.py
+++ views.py
@@ -1,6 +1,9 @@
 import webapp2
+from ai import *
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello, World!')
+        # self.response.write('Hello, World! Please bear with me for a little while I get this up and running')
+        template = JINJA_ENVIRONMENT.get_template('index.html')
+        self.response.write(template.render(template_values))
