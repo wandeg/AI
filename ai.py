eec231b39222df88d918106c50a429300c1a4b0b
wandeg <jmsmaringa@gmail.com>
2014-09-26 13:57:21 +0300

    Tiny (BUGGY) changes

--- ai.py
+++ ai.py
@@ -1,18 +1,19 @@
 import webapp2
 import views
-
+import logging
 from google.appengine.api import users
 from google.appengine.ext import ndb
 
 import jinja2
-
+import os
+import urllib
 
 
 JINJA_ENVIRONMENT = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
     autoescape=True)
-
+logging.info(JINJA_ENVIRONMENT)
 
 application = webapp2.WSGIApplication(
 	[
--- views.py
+++ views.py
@@ -1,9 +1,18 @@
 import webapp2
-from ai import *
-
+import logging
+import ai
+import jinja2
+import os
+# from ai import JINJA_ENVIRONMENT
+JINJA_ENVIRONMENT = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+logging.info(dir(ai))
 class MainPage(webapp2.RequestHandler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         # self.response.write('Hello, World! Please bear with me for a little while I get this up and running')
         template = JINJA_ENVIRONMENT.get_template('index.html')
-        self.response.write(template.render(template_values))
+        logging.info(dir(template))
+        self.response.write(template)
